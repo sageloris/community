b636283acacca4eedacad063f74f00f7dcd2b7c6
sageloris <sageloris@outlook.com>
2020-03-28 21:08:29 +0800

    add index and bootstrap

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 ## bjarnelinusCommunity
+我的个人网站，包括blog，分享一些学习资料，game Community,还有杂七杂八的想法。。。
 
 ## 资料
 
--- src/main/java/com/bjarnelinuscommunity/community/controller/IndexController.java
+++ src/main/java/com/bjarnelinuscommunity/community/controller/IndexController.java
@@ -12,15 +12,13 @@
 package com.bjarnelinuscommunity.community.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class HelloController {
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name = "name") String name, Model model){
-        model.addAttribute("name", name);
-        return "hello";
+public class IndexController {
+    @GetMapping("/")
+    public String index(){
+
+        return "index";
     }
 }
